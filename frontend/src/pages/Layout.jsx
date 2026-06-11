e98fbd788d6501e36be25874bb5c163f9bf5b3b0
Vishnu Parihar <vishnuparihar.delhi5@gmail.com>
2026-06-11 14:58:38 +0530

    Remove max-w-100 class from Layout component

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
     <div className="flex h-screen bg-linear-to-br from-slate-50 via-white to-indigo-50/30">
       <Sidebar />
       <main className="flex-1 overflow-y-auto">
-        <div className="p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8 max-w-100 mx-auto">
+        <div className="p-4 pt-16 sm:p-6 sm:pt-6 lg:p-8">
           <Outlet />
         </div>
       </main>
